2f1e711996af0016cb3782cb1a5f9bf092972de1
Miranda Ramirez <mxr17e@acu.edu>
2020-01-21 14:51:40 -0600

    Dice project part 1

--- D20ForNoobs/ViewController.swift
+++ D20ForNoobs/ViewController.swift
@@ -10,11 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    func rollDice() {
+        let rolledNumber = Int.random(in: 1...20)
+        
+        let imageName = "d\(rolledNumber)"
+        
+        diceImageView.image = UIImage(named: imageName)
+    }
+    
+    override func motionEnded (_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        rollDice()
+    }
+    
+    @IBAction func rollDiceFromButton() {
+        rollDice()
+    }
+    
 }
 
